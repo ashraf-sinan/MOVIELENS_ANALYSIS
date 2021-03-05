93558ae3348fa2224c609b61e721641162462959
Ashraf Sinan <asashrafali@gmail.com>
2021-03-05 00:20:41 +0000

    exported to pycode

--- SparkPractical.py
+++ SparkPractical.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar  4 21:26:38 2021
+#!/usr/bin/env python
+# coding: utf-8
 
-@author: ashraf sinan
-"""
+# In[58]:
+
 
 import pyspark 
-from pyspark.sql import SQLContext
+import random
+from pyspark.sql import SQLContext, functions as func, types as typ
+
 sc = pyspark.SparkContext.getOrCreate() 
 ## Read the data into memory
 
@@ -19,52 +19,84 @@ links = sqlContest.read.csv('ml-latest-small/links.csv', header=True)
 ratings = sqlContest.read.csv('ml-latest-small/ratings.csv', header=True)
 tags = sqlContest.read.csv('ml-latest-small/tags.csv', header=True)
 #print(mv1.first())
+##Convert the timestamp to date
 
+ratings = ratings.withColumn('RatingDate', func.date_format(ratings.timestamp.
+                                                       cast(dataType= typ.LongType()).
+                                                       cast(dataType= typ.TimestampType()),
+                                                       "yyyy-MM-dd"))
+
+tags = tags.withColumn('tagDate', func.date_format(tags.timestamp.
+                                                       cast(dataType= typ.LongType()).
+                                                       cast(dataType= typ.TimestampType()),
+                                                   "yyyy-MM-dd"))
+
+## Add geners columns using binary encoding for each category
+genersLabels = ["Action", "Adventure", "Animation", "Children's", "Comedy"
+                "Crime", "Documentary","Drama", "Fantasy", "Film-Noir",
+                "Horror", "Musical", "Mystery", "Romance", "Sci-Fi",
+                "Thriller", "War", "Western"]
+
+movies1 = movies
+for label in genersLabels:
+    movies = movies.withColumn(label, movies["genres"].like("%" + label +"%"))
+
+geners = movies.select("genres").distinct()
+
+geners.show()
+#print(geners.count())
 ##Persist the data in memory
 movies.persist()
 links.persist()
 ratings.persist()
 tags.persist()
 
+#Covert timestamp seconds to date
+
+
 #print(ratings.count())
 ## Basic Requirements
 
 def searchUserById(id_):
     filtered = ratings.where("userId =" + str(id_))
-    print("The user with id:" + str(id_) + "  Have watched " + str(filtered.count()) +" MOVIES")
-    
-
+    filtered.show()
+    filtered = filtered.join(movies, on=['movieId'], how ='inner')
+    filtered.show()
+    return filtered
 def searchUsersById(ids):
     
-    return 0
+    return 
 
 def searchMovieById(id):
-    
-    return 0
+    filteredMovies = movies.where("movieId = " + str(id))
+    filteredMovies.show()
+    return 
 
 def searchMovieByTitle(title):
-    
-    return 0
+    filteredMovies = movies.where("title like \'%" + str(title) + "%\'")
+    filteredMovies.show()
+    return 
 
 def searchGenre(gen):
-    
-    return 0
+    filteredMovies = movies.where("genres like \'%" + str(gen) + "%\'")
+    filteredMovies.show()
+    return filteredMovies
 
 def searchGenres(gens):
     
-    return 0
+    return
 
 def searchMoviesByYear(year):
     
-    return 0
+    return
 
 def listTopNRated(n):
     
-    return 0
+    return
 
 def listTopNWatched(n):
     
-    return 0
+    return
 
 
 ## Intermediate Requirements:
@@ -90,6 +122,21 @@ def recommendMovie(userId):
     
     return 0
 
-
-searchUserById(2)
+searchMovieById(2)
+searchGenre("Action")
+searchMovieByTitle("Jum")
+searchUserById(5)
 searchUserById(22)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
