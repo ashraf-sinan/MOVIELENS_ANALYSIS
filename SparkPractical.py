0a9f90c8b9f7a720f10d6c1800e9ea652b7319f9
Karl <karlsykes@hey.com>
2021-03-17 11:08:11 +0000

    Counting genres watched

--- SparkPractical.py
+++ SparkPractical.py
@@ -7,6 +7,7 @@
 import pyspark 
 import random
 from pyspark.sql import SQLContext, functions as func, types as typ
+from pyspark.sql.functions  import explode, split, avg, col, desc, asc
 
 sc = pyspark.SparkContext.getOrCreate() 
 ## Read the data into memory
@@ -31,25 +32,32 @@ tags = tags.withColumn('tagDate', func.date_format(tags.timestamp.
                                                        cast(dataType= typ.TimestampType()),
                                                    "yyyy-MM-dd"))
 
-## Add geners columns using binary encoding for each category
-genersLabels = ["Action", "Adventure", "Animation", "Children's", "Comedy"
+## Add genres columns using binary encoding for each category
+genresLabels = ["Action", "Adventure", "Animation", "Children's", "Comedy"
                 "Crime", "Documentary","Drama", "Fantasy", "Film-Noir",
                 "Horror", "Musical", "Mystery", "Romance", "Sci-Fi",
                 "Thriller", "War", "Western"]
 
 movies1 = movies
-for label in genersLabels:
+for label in genresLabels:
     movies = movies.withColumn(label, movies["genres"].like("%" + label +"%"))
 
-geners = movies.select("genres").distinct()
+#genres = movies.select("genres").distinct()
 
-geners.show()
-#print(geners.count())
+#genres.show()
+#print(genres.count())
 ##Persist the data in memory
 movies.persist()
 links.persist()
 ratings.persist()
 tags.persist()
+
+movies.show()
+tags.show()
+ratings.show()
+links.show()
+
+
 
 #Covert timestamp seconds to date
 
@@ -57,33 +65,76 @@ tags.persist()
 #print(ratings.count())
 ## Basic Requirements
 
-def searchUserById(id_):
-    filtered = ratings.where("userId =" + str(id_))
-    filtered.show()
-    filtered = filtered.join(movies, on=['movieId'], how ='inner')
-    filtered.show()
+# Search user by id, show the number of movies that he/she has watched
+def searchUserByIdMovies(id):
+    # Get distinct userID in ratings DF
+    # Count distinct movie IDs for userID
+    
+    # Get distinct userID in tags DF
+    # Count distinct movie IDs for userID
+
+    # Combine counts from above two 
+
     return filtered
+
+# Count the number of movies each userId has watched
+def countWatchedMovies():
+    # Group by userId and movieId
+    tagged_count = tags.groupBy("userId", "movieId").agg(func.countDistinct("tag").alias("taggedCount"))
+    rated_count = ratings.groupBy("userId", "movieId").agg(func.countDistinct("rating").alias("ratedCount"))
+
+    # Combine rows from ratings and tags tables
+    rated_tagged_joined = tagged_count.join(rated_count, on=['userId', "movieId"], how ='fullouter')
+
+    # Count unique number of movieIds per userId
+    movies_watched_count = rated_tagged_joined.groupBy("userId").agg(func.countDistinct("movieId").alias("moviesWatched"))
+    
+    return movies_watched_count
+
+# Count the number of genres each userId has watched
+def count_watched_genres():
+    # Group by userId and movieId
+    tagged_count = tags.groupBy("userId", "movieId").agg(func.countDistinct("tag").alias("taggedCount"))
+    rated_count = ratings.groupBy("userId", "movieId").agg(func.countDistinct("rating").alias("ratedCount"))
+
+    # Combine rows from ratings and tags tables
+    movies_watched = tagged_count.join(rated_count, on=['userId', "movieId"], how ='fullouter')
+
+    # Combine with genres
+    movies_watched_genres = movies_watched.join(movies, on=["movieId"], how='leftouter')
+    
+    # Split genres into array column
+    movies_watches_split_genres = movies_watched_genres.select(movies_watched_genres.userId, split(col("genres"), "[|]").alias("genresArray"))
+    movies_watches_split_genres.show()
+    
+    # Explode genres array into rows
+    movies_watched_explode_genres = movies_watches_split_genres.select(movies_watches_split_genres.userId, explode("genresArray").alias("Genre"))
+    movies_watched_explode_genres.show()
+
+    # Group by userId, counting distinct number of genres watched
+    genres_watched = movies_watched_explode_genres.groupBy("userId").agg(func.countDistinct("Genre").alias("genresWatched"))
+    genres_watched.show()
+    
+    return genres_watched
+
+def get_watch_count(id):
+    movies_watched_count.where("userId = " + id).show()
+
 def searchUsersById(ids):
     
     return 
 
 def searchMovieById(id):
-    filteredMovies = movies.where("movieId = " + str(id))
-    filteredMovies.show()
-    return 
+    return filtered
 
 def searchMovieByTitle(title):
-    filteredMovies = movies.where("title like \'%" + str(title) + "%\'")
-    filteredMovies.show()
+    
     return 
 
 def searchGenre(gen):
-    filteredMovies = movies.where("genres like \'%" + str(gen) + "%\'")
-    filteredMovies.show()
-    return filteredMovies
+    return
 
 def searchGenres(gens):
-    
     return
 
 def searchMoviesByYear(year):
@@ -122,21 +173,16 @@ def recommendMovie(userId):
     
     return 0
 
-searchMovieById(2)
-searchGenre("Action")
-searchMovieByTitle("Jum")
-searchUserById(5)
-searchUserById(22)
+
+# Get count of movies watched
+# movies_watched_count = countWatchedMovies()
+# movies_watched_count.persist()
+# get_watch_count("2")
+count_watched_genres()
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+# searchMovieById(2)
+# searchGenre("Action")
+# searchMovieByTitle("Jum")
+# searchUserById(5)
+# searchUserById(22)
