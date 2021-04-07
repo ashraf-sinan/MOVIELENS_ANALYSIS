b1eabcb2fc9696314c5e097f722a8e03294ff34c
Ashraf Sinan <asashrafali@gmail.com>
2021-04-07 12:05:32 +0100

    merged version

--- ControlPanel.py
+++ ControlPanel.py
@@ -1,13 +1,17 @@
+
+import findspark
+spark = findspark.init()
+
 import pyspark 
 import random
 from pyspark.sql import SQLContext, functions as func, types as typ
-from pyspark.sql.functions  import explode, split, avg, col, desc, asc, count, countDistinct, substring
+from pyspark.sql.functions  import explode, split, avg, col, desc, asc, count, countDistinct, substring, rtrim
 from pyspark.ml.recommendation import ALS
 from pyspark.ml.evaluation import RegressionEvaluator
 import matplotlib.pyplot as plt
 import SparkPractical1 as funcs1
 import HelperFunctions as helperFuncs
-
+import SparkPractical_module2 as module2
 sc = pyspark.SparkContext.getOrCreate() 
 ## Read the data into memory
 
@@ -34,7 +38,7 @@ def clean_data(ratings, movies):
     # Drop NA values
     # Convert data Type of columns intro integer and float
     clean_ratings = ratings.na.drop()\
-    .withColumn('RatingDate', func.date_format(ratings.timestamp.
+    .withColumn('watching_date', func.date_format(ratings.timestamp.
                                                            cast(dataType= typ.LongType()).
                                                            cast(dataType= typ.TimestampType()),
                                                            "yyyy-MM-dd"))\
@@ -47,7 +51,7 @@ def clean_data(ratings, movies):
     
     clean_movies = movies.na.drop()\
     .withColumn("movieId", movies.movieId.cast(dataType = typ.IntegerType()))\
-    .withColumn("production_year", substring(col("title"), -5, 4))
+    .withColumn("production_year", substring(rtrim(col("title")), -5, 4))
     
     #Split the genres column into rows based on the number of the separator | 
 
@@ -64,13 +68,17 @@ def clean_data(ratings, movies):
 def controlPanel():
     
     #Select the dataset
-    dataPath = SMALL_DATASET_PATH
+    data_path = SMALL_DATASET_PATH 
+    
+    data_select = int(input("Please enter the number (1) to select the small dataset or (2) to use the big dataset:").strip())
+    if data_select == 2:
+        data_path = DATASET_PATH
     
     #Load the datasets
-    ratings, movies, tags = load_data(dataPath)
+    ratings, movies, tags = load_data(data_path)
     
     #Clean the dataset
-    ratings_clean, movies_clean, movies_genres = clean_data(ratings, movies)
+    ratings, movies, movies_genres = clean_data(ratings, movies)
     #ratings.show()
     #movies.show()
     
@@ -82,7 +90,12 @@ def controlPanel():
     ratings.persist()
     movies_genres_watched.persist()
 
-    
+    ## Prepare the recommendation system
+    if data_select != 2:
+        als_model = module2.train_als_model(ratings)
+        print("Recommendations system is ready now....")
+    else:
+        print("Recommendation can work only with the small data because of the limited resources!")
     #Set the number of records to show
     N = 7
     
@@ -92,28 +105,30 @@ def controlPanel():
         print("1- Search user | 2-Search Users | 3-Search Genre | 4-Search Genres")
         print("5- Count movies watched by ID | 6- Count genres watched by ID | 7- Search movies watched by IDs")
         print("8- Summarise movie average rating and watch count by id| 9- Summarise movie average rating and watch count by title")
-        print("10- Search movies in genres| 11- List top rating movies | 12- search movies by year")
-        print("13- Show user's favourite genres | 14- compare two users' taste | 15- Show visualizations ")
-        print("16- Recommend Movies for user | 17- Exit")
+        print("10- Search movies in genres| 11- List top wathed movies | 12- List most rated Movies | 13- search movies by year")
+        print("14- Show user's favourite genres | 15- compare two users' taste | 16- Show visualizations ")
+        print("17- Recommend Movies for user | 18- Print Data Sumary | 19- Exit")
         
         try:
             choice = int(input('Enter your choice number from the list above:').strip())
             
-            if choice == 1:
+            if choice == 1: ## Search user by ID (Ashraf)
                 user_id = helperFuncs.enterId("user ")
-                #search_user_by_id(user_id, ratings, movies).show()
+                module2.show_user_details(user_id, movies_genres, ratings, movies) 
 
-            elif choice == 2:   
-                usersIds = helperFuncs.enterUsersIds()
-                #search_users_by_ids(usersIds, ratings, movies).show()
+            elif choice == 2: ## Search many Users by ID (Ashraf)
+                users_ids = helperFuncs.enterUsersIds()
+                for user_id in users_ids:
+                    module2.show_user_details(user_id, movies_genres, ratings, movies)
 
-            elif choice == 3:   # Search for movies of specified genre
+            elif choice == 3:   # Search for movies of specified genre(Ashraf)
                 genre = input("Please enter the genre name:").strip()
-                #search_genre(genre, movies).show()
+                module2.show_genre_details(genre, movies)
 
-            elif choice == 4:   # Search for movies of specified genres
+            elif choice == 4:   # Search for movies of specified genres (Ashraf)
                 genres = helperFuncs.enterGenres()
-                #search_genres(genres, movies_genres, movies).show()
+                for genre in genres:
+                    module2.show_genre_details(genre, movies)
 
             elif choice == 5:   # Count number of movies user has watched (Karl)
                 userId = helperFuncs.enterId("user ")
@@ -139,31 +154,63 @@ def controlPanel():
                 genres = helperFuncs.enterGenres()
                 funcs1.search_movies_in_genres(movies_genres_watched, genres).show(100)
 
-            elif choice == 11:  # Show top n rated movies 
-                n = int(input("please enter the number of top watching movies to show:").strip())
-                #list_top_watched(ratings, movies).show(n)
+            elif choice == 11:  # Show top n rated movies (Ashraf)
+                n = int(input("please enter the number of top wacthed movies to show:").strip())
+                module2.list_top_watched(ratings, movies).show(n)
+            
+            elif choice == 12:  # Search movie by year(Ashraf)
+                n = input("Pplease enter the number of top rated movies to show:")
+                module2.list_top_rated(ratings, movies).show(n)
             
             
-            elif choice == 12:  # Search movie by year
-                year = input("Please enter a year between 1750 - 2018")
-                #search_movies_by_year(year, movies).show()
+            elif choice == 13:  # Search movie by year(Ashraf)
+                year = input("Please enter a year between 1750 - 2018:  ")
+                module2.search_movies_by_year(year, movies).show()
 
-            elif choice == 13:  # Find favourite genre of user (Karl)
+            elif choice == 14:  # Find favourite genre of user (Karl)
                 user_id = helperFuncs.enterId("user ")
                 funcs1.search_favourite_genre(user_id, movies_genres_watched).show()
 
-            elif choice == 14:  # Compare user tastes
+            elif choice == 15:  # Compare user tastes (Ashraf)
                 user1_Id = helperFuncs.enterId("First user ")
                 user2_Id = helperFuncs.enterId("Second user ")
-                #compareTastes(user1_Id, user2_Id, movies_genres, ratings_clean, movies_clean).show()
+                summary, u1_diff, u2_diff, u1_and_u2_like, u1_and_u2_dislike, u1_like_u2_dislike, u1_dislike_u2_like = ( 
+                module2.compareTastes(user1_Id, user2_Id, movies_genres, ratings, movies))
+                
+                print("Comparing the Tastes of user:" + 
+                     str(user1_Id) + " , and user:" + str(user2_Id) + " :" ) 
+                print("Both users are watching the following genres, the table below show compare there watching times and ratings:")
+                summary.show()
+                print("The next Genres are favourite for both user:" + 
+                     str(user1_Id) + " , and user:" + str(user2_Id) + " :" )
+                u1_and_u2_like.show()
+                print("The next Genres are unfavourite for both user:" + 
+                     str(user1_Id) + " , and user:" + str(user2_Id) + " :" )
+                u1_and_u2_dislike.show()
+                print("The next Genres are preffered by user:" + 
+                     str(user1_Id) + " , but disliked by user:" + str(user2_Id) + " :" )
+                u1_like_u2_dislike.show()
+                print("The next Genres are preffered by user:" + 
+                     str(user1_Id) + " , but disliked by user:" + str(user2_Id) + " :" )
+                u1_dislike_u2_like.show()
+                
+                print("User " + str(user1_Id) + " watched " + str(u1_diff.count()) + " Genres that User " + str(user2_Id) + " have not watched :")
+                u1_diff.show()
+                print("User " + str(user2_Id) + " watched " + str(u2_diff.count()) + " Genres that User " + str(user1_Id) + " have not watched :")
+                u2_diff.show()
 
-            elif choice == 15:  # Visualise (Karl)
+            elif choice == 16:  # Visualise (Karl)
                 funcs1.select_visualisations(movies_genres_watched)
 
-            elif choice == 16:
-                print("Recommend movies")
+            elif choice == 17: ## Recommend movies (Ashraf)
+                user_id = module2.enterId("user ")
+                print("The top 10 recommended movies for this user are:")
+                rec_movies = module2.recommend_movies(als_model, user_id, 10, movies)
+                rec_movies.show()
+            elif choice == 18: ## Summarize Data (Ashraf)
+                module2.summarize_all_data(ratings, movies, movies_genres)
 
-            elif choice == 17:
+            elif choice == 19:
                 break
             
         except ValueError:
